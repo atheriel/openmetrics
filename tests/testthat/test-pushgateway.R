931c342188b5c33c1e907294acba51f3b35763b7
Aaron Jacobs <aaron.jacobs@crescendotechnology.com>
2020-07-13 16:29:29 +0000

    Add basic local tests against the Pushgateway.

---
+++ tests/testthat/test-pushgateway.R
@@ -0,0 +1,33 @@
+testthat::test_that("Pushgateway interaction works as expected", {
+  reg <- registry()
+  register_default_metrics(reg)
+
+  counter <- counter_metric("count", "Custom counter.", registry = reg)
+  counter$inc(5)
+
+  testthat::skip_on_cran()
+  testthat::skip_on_ci()
+
+  testthat::expect_silent(
+    push_to_gateway(
+      "http://localhost:9091", job = "openmetrics-tests-1", registry = reg
+    )
+  )
+  testthat::expect_silent(
+    push_to_gateway(
+      "http://localhost:9091", job = "openmetrics-tests-1", instance = "2",
+      registry = reg
+    )
+  )
+  testthat::expect_silent(
+    delete_from_gateway(
+      "http://localhost:9091", job = "openmetrics-tests-1", instance = "2",
+      registry = reg
+    )
+  )
+  testthat::expect_silent(
+    delete_from_gateway(
+      "http://localhost:9091", job = "openmetrics-tests-1", registry = reg
+    )
+  )
+})
